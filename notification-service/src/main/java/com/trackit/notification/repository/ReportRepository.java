01a21fdb5491fdcf761d2f9919011009d3bcfcd1
Anbumani P <anbuarul401@gmail.com>
2026-05-24 09:44:49 +0530

    Delete notification-service directory

--- notification-service/src/main/java/com/trackit/notification/repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trackit.notification.repository;
-
-import com.trackit.notification.entity.Report;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReportRepository extends MongoRepository<Report, String> {
-}
